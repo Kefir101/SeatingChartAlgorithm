64d870e2f849ed4da592893bfb0791d4c5d02f54
Kefir101 <kefir.sade.bar@gmail.com>
2021-09-06 11:13:56 -0700

    master

--- main.js
+++ main.js
@@ -4,30 +4,31 @@ class Student {
     this.name = name_;
     this.frontPreference = frontPreference_;
     this.backPreference = backPreference_;
+    this.sitNextTo = sitNextTo_;
+    this.doNotSitNextTo = doNotSitNextTo_;
+    this.happy = "";
+    this.sad = "";
   }
 }
 const gridSize = 5;
 let positionScore = 0;
 let previousPositionScore = positionScore;
-let students = new Array(2 * gridSize); //2 x 5 grid
+let students = []; //2 x 5 grid
+let studentCount = 2 * gridSize;
 const preferenceCount = 2;
 function setup() {
-  createCanvas(400, 400);
-  background(255);
-  for (let i = 0; i < students.length; i++) {
+  createCanvas(1000, 1000);
+  for (let i = 0; i < studentCount; i++) {
     let sidePreference = Math.random() < 0.5;
     students.push(new Student("Bob" + i, sidePreference, !sidePreference, new Array(preferenceCount), new Array(preferenceCount)));
   }
-  for (let i = 0; i < students.length; i++) {
+  for (let i = 0; i < studentCount; i++) {
+    let student = students[i];
     let indexes = studentPreferenceIndexes(preferenceCount, i);
-    let sitNextTo = new Array(preferenceCount);
-    let doNotSitNextTo = new Array(preferenceCount);
     for (let j = 0; j < preferenceCount; j++) {
-      sitNextTo[j] = students[indexes[j]].name;
-      doNotSitNextTo[j] = students[indexes[j + preferenceCount]].name;
+      student.sitNextTo[j] = students[indexes[j]].name;
+      student.doNotSitNextTo[j] = students[indexes[j + preferenceCount]].name;
     }
-    students[i].sitNextTo = sitNextTo;
-    students[i].doNotSitNextTo = doNotSitNextTo;
   }
   randomizePositions();
   countScore();
@@ -36,27 +37,31 @@ function setup() {
 function draw() {
   let x = 50, y = 50;
   textSize(10);
-  fill(0);
-  for (let s in students) {
-    text(s.name, x, y);
-    text(s.sitNextTo.toString(), x - 20, y + 20);
-    text(s.doNotSitNextTo.toString(), x - 20, y + 40);
-    text("Happy: " + s.happy, x - 20, y + 60);
-    text("Sad: " + s.sad, x - 20, y + 80);
+  stroke(0);
+  for (let i = 0; i < students.length; i++) {
+    let student = students[i]
+    text(student.name, x, y);
+    text(student.sitNextTo, x - 20, y + 20);
+    text(student.doNotSitNextTo, x - 20, y + 40);
+    text("Happy: " + student.happy, x - 20, y + 60);
+    text("Sad: " + student.sad, x - 20, y + 80);
     x += 100;
     if (x >= 500) {
       x = 50;
       y += 100;
     }
   }
+  noLoop();
 }
 function studentPreferenceIndexes(preferenceCount_, selfIndex) {
-  let indexes = new Array(2 * preferenceCount_);
+  let indexes = [];
+  let indexesLengh = 2 * preferenceCount_;
   let duplicates = false;
   do {
     duplicates = false;
-    for (let j = 0; j < indexes.length; j++) {
-      indexes[j] = (int)(Math.random() * students.length);
+    indexes = [];
+    for (let j = 0; j < indexesLengh; j++) {
+      indexes.push(Math.trunc(Math.random() * students.length));
     }
     for (let j = 0; j < indexes.length; j++) {
       for (let k = 0; k < indexes.length; k++) {
@@ -80,36 +85,37 @@ function swapStudents() {
 function countScore() {
   positionScore = 0;
   for (let i = 0; i < students.length; i++) {
-    let frontPreference = students[i].frontPreference;
-    let backPreference = students[i].backPreference;
-    let sitNextTo = students[i].sitNextTo;
-    let doNotSitNextTo = students[i].doNotSitNextTo;
+    let s = students[i];
+    let frontPreference = s.frontPreference;
+    let backPreference = s.backPreference;
+    let sitNextTo = s.sitNextTo;
+    let doNotSitNextTo = s.doNotSitNextTo;
     let closeIndexes = [i + 1, i - 1, i + gridSize, i - gridSize];
-    let closeNames = new Array(preferenceCount * 2);
+    let closeNames = [];
+    let nameCount = preferenceCount * 2;
     for (let j = 0; j < closeIndexes.length; j++) {
       let pos = closeIndexes[j];
       if (pos < 0 || pos >= students.length) continue;
       if (pos == i + 1 && i + 1 % gridSize == 0) continue;
       if (pos == i - 1 && i % gridSize == 0) continue;
-      closeNames[j] = students[pos].name;
+      closeNames.push(students[pos].name);
+      if(closeNames.length == 2) break;
     }
-    let closeNamesTemp = [];
-    for (let s in closeNames)
-      if (s != null)
-        closeNamesTemp.add(s);
-    closeNames = closeNamesArrayList.toArray(new Array(closeNamesArrayList.size()));
-    students[i].happy.setLength(0);
-    students[i].sad.setLength(0);
-    for (let closeName in closeNames) {
-      for (let k = 0; k < preferenceCount; k++) {
-        if (sitNextTo[k].equals(closeName)) {
-          positionScore += 100;
-          students[i].happy.append(closeName).append(", ");
-        }
-        if (doNotSitNextTo[k].equals(closeName)) {
-          positionScore -= 200;
-          students[i].sad.append(closeName).append(", ");
-        }
+    // let closeNamesTemp = [];
+    // for (let j = 0; j < nameCount; j++)
+    //   if (closeNames[j] != null)
+    //     closeNamesTemp.push(closeNames[j]);
+    // closeNames = closeNamesTemp;
+    s.happy = "";
+    s.sad = "";
+    for (let j = 0; j < closeNames.length; j++) {
+      let closeName = closeNames[j];
+      if (sitNextTo.includes(closeName)) {
+        positionScore += 100;
+        s.happy += closeName + ", "; 
+      }else if (doNotSitNextTo.includes(closeName)) {
+        positionScore -= 200;
+        s.sad += closeName + ", ";
       }
     }
     if (i < gridSize && frontPreference) positionScore += 5;
@@ -134,22 +140,38 @@ function loopChanges() {
   //            positionScoreChange = positionScore - previousPositionScore;
   //            previousPositionScore = positionScore;
   //        }
-  for (let i = 0; i < 100000; i++) {
-    positionScoreChange = swapCountAndScore();
-    if (positionScoreChange == 0) {
-      for (let j = 0; j < 100000; j++) {
-        positionScoreChange = swapCountAndScore();
-        if (positionScoreChange > 0) {
-          break;
-        }
-      }
-      printStudents();
-      console.log(positionScore);
-      break;
-    }
+  // let storeStudents = [];
+  // let iterations = 1000;
+  // for (let i = 0; i < 100000; i++) {
+  //   positionScoreChange = swapCountAndScore();
+  //   for (let j = 0; j < iterations; j++) { //run loop again
+  //     positionScoreChange = swapCountAndScore();
+  //     if (positionScoreChange > 0) { //if for that entire loop not a single arrangment is better
+  //       storeStudents = students;
+  //       break; //restart if isn't the best
+  //     }
+  //     if(j == iterations-1){
+  //       printStudents(storeStudents);
+  //       console.log(positionScore);
+  //       break;
+  //     }
+  //   }
+  // }
+  let storeStudents = [];
+  let storeScores = [];
+
+  for(let i = 0; i < 100000; i++){
+    swapStudents();
+    countScore();
+    storeStudents.push(students);
+    storeScores.push(positionScore);
   }
+  var indexOfMaxValue = storeScores.reduce((iMax, x, i, storeScores) => x > storeScores[iMax] ? i : iMax, 0); //google
+  console.log(storeStudents[indexOfMaxValue])
+  console.log(storeScores[indexOfMaxValue])
+  printStudents(storeStudents[indexOfMaxValue])
 }
-function printStudents() {
+function printStudents(students) {
   let names = "";
   let happy = "";
   let frontPref = "";
@@ -185,6 +207,5 @@ function shuffleArray(array) {
     [array[currentIndex], array[randomIndex]] = [
       array[randomIndex], array[currentIndex]];
   }
-
   return array;
 }
